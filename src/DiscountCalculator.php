49686bb1bc0195f51d88f4953ce7c0e4813aad75
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-11-15 19:19:14 -0300

    clear code

--- src/DiscountCalculator.php
+++ src/DiscountCalculator.php
@@ -6,11 +6,14 @@ class DiscountCalculator
 {
     public function calculate(Budget $budget): float
     {
+        $aux = $budget->value;
+
         if ($budget->items_qtd > 5) {
             $aux = $budget->value * 0.1;
+        }
 
-        } else {
-            $aux = $budget->value;
+        if($budget->value > 500) {
+            $aux = $budget->value * 0.05;
         }
 
         $ret = $aux;
